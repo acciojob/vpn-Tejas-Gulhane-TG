ac9b530b6ba6710240452f44246626b3e37afc95
Tejas Gulhane <tgulhane28@gmail.com>
2023-06-03 18:05:07 +0530

    test

--- src/main/java/com/driver/controllers/UserController.java
+++ src/main/java/com/driver/controllers/UserController.java
@@ -1,6 +1,6 @@
 package com.driver.controllers;
 import com.driver.model.User;
-import com.driver.services.Impl.UserServiceImpl;
+import com.driver.services.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
     @Autowired
     UserServiceImpl userService;
-
     @PostMapping("/register")
     public ResponseEntity<Void> registerUser(@RequestParam String username, @RequestParam String password, @RequestParam String countryName) throws Exception{
         //create a user of given country. The originalIp of the user should be "countryCode.userId" and return the user. Note that right now user is not connected and thus connected would be false and maskedIp would be null
--- src/main/java/com/driver/services/Impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/Impl/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package com.driver.services.Impl;
+package com.driver.services.impl;
 
 import com.driver.model.Country;
 import com.driver.model.CountryName;
